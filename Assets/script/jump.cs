5044f1e29b91b0f90b4e2abb852230bf337ba2ec
Akshat <akshatsbg@gmail.com>
2018-07-05 02:22:24 -0700

    Initial commit

---
+++ Assets/script/jump.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jump : MonoBehaviour {
+    public int jumpValue;
+   public bool Jump=true;
+  public  bool grounded;
+    Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = gameObject.GetComponent<Rigidbody>();
+        Jump = true;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Jump == true && grounded == true)
+        {
+            Vector3 playerPos = gameObject.transform.position;
+            transform.Translate(0f, 500 * Time.deltaTime, 0f);
+
+            Jump = false;
+            grounded = false;
+
+
+            StartCoroutine(Wait(3));
+        }
+        
+	}
+    IEnumerator Wait(int time)
+    {
+        Jump = false;   
+        yield  return new WaitForSeconds(time);
+        Jump = true;
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = false;
+        }
+    }
+}
---
+++ Assets/script/obstrucleAvoider.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets;
+
+public class obstrucleAvoider : MonoBehaviour {
+    bool forward = true;
+    public int speed;
+    Rigidbody rb;
+    Vector3 playerInitialPos;
+    bool up=false;
+
+	// Use this for initialization
+	void Start () {
+        rb = gameObject.GetComponent<Rigidbody>();
+        playerInitialPos = gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (forward)
+        {
+            Vector3 playerPos = gameObject.transform.position;
+            rb.MovePosition(playerPos + Vector3.right/speed);
+
+        }
+        if (up == true && gameObject.transform.position.z != playerInitialPos.z)
+        {
+            Vector3 playerPos = gameObject.transform.position;
+            rb.MovePosition(playerPos + Vector3.forward);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        rb.useGravity = false;
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == ("obstrucle"))
+        {
+
+            forward = false;
+            Vector3 playerPos = gameObject.transform.position;
+            rb.MovePosition(playerPos - Vector3.forward / 2);
+
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "obstrucle")
+        {
+            Vector3 obstruclePos = other.gameObject.transform.position;
+            forward = true;
+            rb.useGravity = true;
+        }
+    }
+}
